80cbd18947b48e73e04cea860ff9cc7a5b89e31d
Ryan McFadden <ryanmcfadden123@gmail.com>
2020-04-10 00:02:45 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,3 +8,7 @@ doc = Nokogiri::HTML(html)
 doc.css(".headline-26OIBN").text
 
 courses = doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlinemobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+
+courses.each do |course|
+  puts course.text.strip
+end
